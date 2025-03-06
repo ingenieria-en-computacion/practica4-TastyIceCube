153c97e17306d2c27406e21b346b94ae52d3ac15
Salinas Ruiz Cristian Daniel <danielsalinas544@gmail.com>
2025-03-06 10:39:31 -0600

    ejercicos en clase

--- ejercicios/ejercicio1.c
+++ ejercicios/ejercicio1.c
@@ -2,24 +2,26 @@
 #include <stdlib.h>
 
 int main() {
-    int n;
+    int TamArr;
     printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &TamArr);
     
-
     int *arr;
+    arr = (int*)malloc(TamArr *sizeof(int));
     if (arr == NULL) {
         printf("Error: No se pudo asignar memoria.\n");
         return 1;
     }
 
-    printf("Ingrese %d valores:\n", n);
-    for (int i = 0; i < n; i++) {
-        
+    printf("Ingrese %d valores:\n", TamArr);
+    for (int i = 0; i < TamArr; i++) {
+        scanf("%p", arr+i);
+    
     }
 
-    printf("El arreglo es: ");
-    for (int i = 0; i < n; i++) {
-        
+    printf("El arreglo es: \n");
+    for (int i = 0; i < TamArr; i++) {
+        printf("%d", arr+i);
     }
     printf("\n");
 
